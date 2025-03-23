76a370cefaee5b8ffcd97ff3fc95bf966caa5917
tgothorp <tgothorp@users.noreply.github.com>
2025-03-23 08:14:34 +0000

    Demo BlazorPdf running in maui application

---
+++ BlazorPdf.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPdf.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlazorPdf.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorPdf.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
